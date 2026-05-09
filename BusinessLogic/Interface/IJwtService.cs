85c775467d5e5d8e6ba22077e3cda8d4887d8f12
anurag29310 <anuragsingh29310@gmail.com>
2026-05-09 21:58:20 +0530

    save local changes before merge

---
+++ BusinessLogic/Interface/IJwtService.cs
@@ -0,0 +1,11 @@
+﻿using IdentityService.Models;
+
+namespace IdentityService.BusinessLogic.Interface
+{
+    public interface IJwtService
+    {
+        Task<string> GenerateTokenAsync(UserLoginRequest request);
+
+        Task<bool> RegisterUserAsync(RegisterationRequest request);
+    }
+}
---
+++ Models/Response/AuthResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace IdentityService.Models.Response
+{
+    public class AuthResponse
+    {
+        public string Token { get; set; } = default!;
+    }
+}
